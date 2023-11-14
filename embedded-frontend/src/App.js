7cce7b5a0413e4c0ddadda43a128c4c85bcb5615
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-11-14 21:36:02 +0900

    add 4 charts

--- embedded-frontend/src/App.js
+++ embedded-frontend/src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import Barchart from './Barchart/Barchart';
+import Pichart from './Pichart/Pichart';
+import Radarchart from './Radarchart/Radarchart';
+import Linechart from './Linechart/Linechart';
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <div className='barGraph'>barGraph</div>
-      <div className='piGraph'>piGraph</div>
+      <div className='Graph1' style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <div className='barGraph'>barGraph <Barchart /> </div>
+        <div className='piGraph'>piGraph<Pichart /></div>
+      </div>
+      <div className='Graph2' style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <div className='RadarGraph'>RadarGraph <Radarchart /> </div>
+        
+        
+        <div className='LineGraph'>LineGraph  <Linechart /> </div>
+      </div>
 
     </div>
     // <div>
