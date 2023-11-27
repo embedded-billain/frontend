6d86be24afee730ce8dcd2441e4fe7a6c7b278ab
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-11-27 14:53:45 +0900

    add Mainpage

--- embedded-frontend/src/App.js
+++ embedded-frontend/src/App.js
@@ -5,10 +5,18 @@ import Barchart from './Barchart/Barchart';
 import Pichart from './Pichart/Pichart';
 import Radarchart from './Radarchart/Radarchart';
 import Linechart from './Linechart/Linechart';
+import Mainpage from './Mainpage/Mainpage';
+import Card1 from './Card1/Card1';
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      
+      {/* <div className='Graph0' style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
+        <div className='Total'>Total<Card1 /></div>
+        <div className='Today'>Today</div>
+        <div className='D-Day'>D-Day</div>
+      </div>
       <div className='Graph1' style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
         <div className='barGraph'>barGraph <Barchart /> </div>
         <div className='piGraph'>piGraph<Pichart /></div>
@@ -18,7 +26,7 @@ function App() {
         
         
         <div className='LineGraph'>LineGraph  <Linechart /> </div>
-      </div>
+      </div> */}
 
     </div>
     // <div>
--- embedded-frontend/src/Barchart/Barchart.js
+++ embedded-frontend/src/Barchart/Barchart.js
@@ -3,37 +3,37 @@ import {BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from 'recha
 
 const data = [
     {
-      "name": "Page A",
+      "name": "팀 A",
       "uv": 4000,
       "pv": 2400
     },
     {
-      "name": "Page B",
+      "name": "팀 B",
       "uv": 3000,
       "pv": 1398
     },
     {
-      "name": "Page C",
+      "name": "팀 C",
       "uv": 2000,
       "pv": 9800
     },
     {
-      "name": "Page D",
+      "name": "팀 D",
       "uv": 2780,
       "pv": 3908
     },
     {
-      "name": "Page E",
+      "name": "팀 E",
       "uv": 1890,
       "pv": 4800
     },
     {
-      "name": "Page F",
+      "name": "팀 F",
       "uv": 2390,
       "pv": 3800
     },
     {
-      "name": "Page G",
+      "name": "팀 G",
       "uv": 3490,
       "pv": 4300
     }
